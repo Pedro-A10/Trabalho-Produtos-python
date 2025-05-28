7fc6eeb48e20c36666ee5b01311b8a7c053b70e6
Pedro A. <pedroaugusto732@gmail.com>
2025-05-28 08:18:32 -0300

    importação da exceção

--- Estoque.py
+++ Estoque.py
@@ -1,30 +1,37 @@
+from Estoque_Exception import ProdutoNaoEncontradoNoEstoqueException, EstoqueInsuficienteException, QuantidadeInvalidaException
+
 class Estoque:
     def __init__(self):
         self.produtos = {}
 
     def adicionar_produto(self, codigo, quantidade):
+        if quantidade <= 0:
+            raise QuantidadeInvalidaException("Quantidade deve ser maior que zero")
         if codigo in self.produtos:
             self.produtos[codigo] += quantidade  # Se produto existe, soma quantidade
         else:
             self.produtos[codigo] = quantidade  # Se não existe, cria nova entrada
 
     def remover_produto(self, codigo, quantidade):
-        if codigo in self.produtos:
-            if self.produtos[codigo] >= quantidade:
-                self.produtos[codigo] -= quantidade
-                if self.produtos[codigo] == 0:
-                    del self.produtos[codigo]
-                return True
-            else:
-                print("Quantidade insuficiente em estoque.")
-                return False
-        else:
-            print("Produto não encontrado no estoque.")
-            return False
+        if codigo not in self.produtos:
+            raise ProdutoNaoEncontradoNoEstoqueException("Produto não encontrado no estoque.")
+        if quantidade <= 0:
+            raise QuantidadeInvalidaException("Quantidade deve ser maior que zero")
+        if self.produtos[codigo] < quantidade:
+            raise EstoqueInsuficienteException("Quantidade insuficiente em estoque.")
+        self.produtos[codigo] -= quantidade
+        if self.produtos[codigo] == 0:
+            del self.produtos[codigo]
 
     def consultar_estoque(self, codigo):
         return self.produtos.get(codigo, 0)
 
+    def listar_estoque(self):
+        if not self.produtos:
+            print("Nenhum produto cadastrado no estoque.")
+        else:
+            for codigo, quantidade in self.produtos.items():
+                print(f"Código: {codigo} - Quantidade: {quantidade}")
     def listar_estoque(self):
         if not self.produtos:
             print("Nenhum produto cadastrado no estoque.")
